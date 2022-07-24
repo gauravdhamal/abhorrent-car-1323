2376b59a6adae3f776dd3ca959c2b9be729eec60
Gaurav Dhamal <86916671+gauravdhamal@users.noreply.github.com>
2022-07-24 12:13:31 +0530

    Add files via upload

---
+++ scripts/link.js
@@ -0,0 +1,77 @@
+import {navbar, footer} from '../dependency/navbar.js'
+
+document.querySelector('#body').innerHTML = navbar()
+
+// document.querySelector('#bodybuilder').innerHTML = navbar()
+document.querySelector('footer').innerHTML = footer()
+
+document.querySelector('.smartbuilder').addEventListener('click',function(){
+    window.location.href = `smartbuilder.html`
+})
+
+
+
+
+document.querySelector('.classicbuilder').addEventListener('click',function(){
+    window.location.href = `classicbuilder.html`
+})
+
+// Solutions page
+document.querySelector('.ecommerce').addEventListener('click',function(){
+    window.location.href = `ecommerce.html`
+})
+
+document.querySelector('.professional').addEventListener('click',function(){
+    window.location.href = `professional.html`
+})
+
+document.querySelector('.leadgeneration').addEventListener('click',function(){
+    window.location.href = `leadgeneration.html`
+})
+
+document.querySelector('.emailmarketing').addEventListener('click',function(){
+    window.location.href = `emailmarketing.html`
+})
+
+// Pricing page links
+
+
+document.querySelector('#pricing').addEventListener('click',function(){
+    window.location.href = `pricing.html`
+})
+
+
+// Learn page links
+
+
+document.querySelector('#whatconintel').addEventListener('click',function(){
+    window.location.href = `whatconintel.html`
+})
+
+document.querySelector('.ultoptguide').addEventListener('click',function(){
+    window.location.href = `ultoptguide.html`
+})
+
+document.querySelector('.whatlandingpage').addEventListener('click',function(){
+    window.location.href = `whatlandingpage.html`
+})
+
+// Contact page
+
+document.querySelector('#contact').addEventListener('click',function(){
+    window.location.href = `contact.html`
+})
+
+document.querySelector('#login').addEventListener('click',function(){
+    window.location.href = `login.html`
+})
+
+document.querySelector('.h_free-trial-btn').addEventListener('click',function(){
+    window.location.href = `pricing.html`
+})
+
+
+
+document.querySelector('#navbar').addEventListener('click',function(){
+    window.location.href = 'contact.html'
+})
